40d1cd2aef29ba06f726417fc71d34ba1a4cb856
Faran42 <faranorphys@gmail.com>
2021-02-14 22:08:07 -0300

    Lucas: Frontend, todas as páginas até o momento.

--- src/backend/routes.js
+++ src/backend/routes.js
@@ -10,9 +10,21 @@ router.get('/login', (req, res) => {
     res.render('pages/login/login')
 });
 
-router.post('/cadastrar/funcionario', (req, res) => {
-    const {nome, idade} = req.body
-    res.json({nome, idade});
+router.get('/cadastro', (req, res) => {
+    res.render('pages/cadastro/cadastro')
 });
+
+router.get('/listagem', (req, res) => {
+    res.render('pages/listagem/listagem')
+});
+
+router.get('/sobre', (req, res) => {
+    res.render('pages/sobre/sobre')
+});
+
+// router.post('/cadastrar/funcionario', (req, res) => {
+//     const {nome, idade} = req.body
+//     res.json({nome, idade});
+// });
 
 export default router;
